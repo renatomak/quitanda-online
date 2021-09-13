3bece861202e72e28e6177e73a7168265345b7cf
renatomak <renato.mark.silva@gmail.com>
2021-09-13 09:58:06 -0300

    searchBar

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Carousel from "../components/Carousel";
+import SearchBar from "../components/SearchBar";
 
 const Home = () => {
   return (
     <>
       <Carousel />
+      <main class="flex-fill">
+        <div class="container">
+          <SearchBar />
+        </div>
+      </main>
     </>
   );
 };
